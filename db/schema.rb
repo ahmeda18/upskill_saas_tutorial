98b6c43f96c216be9293bc949aba62e837498bd8
Ahmed Abbas <ahmeda18@hotmail.com>
2017-10-14 08:05:13 +0000

    Added  create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014052352) do
+ActiveRecord::Schema.define(version: 20171014073727) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
-    t.string   "Email"
+    t.string   "email"
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
