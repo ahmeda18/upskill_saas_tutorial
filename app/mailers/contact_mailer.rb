8edc7552f739a051d3d890e50de99c50d98707cc
Ahmed Abbas <ahmeda18@hotmail.com>
2017-10-23 05:00:04 +0000

    more files fixed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'ahmeda18@hotmail.com'
-  
   def contact_email(name, email, body)
     @name = name
     @email = email
-    @bod = body
-    
+    @body = body
     mail(from: email, subject: 'Contact Form Message')
   end
 end
